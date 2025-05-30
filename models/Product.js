a75279a7c4eb72bcb38026e632d64b7844bca8a9
Kalum Gunawardhana <kguna415@gmail.com>
2025-05-30 19:15:35 +0530

    commit

---
+++ models/Product.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize');
+const db = require('../config/database');
+
+const Product = db.define('Product', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.TEXT
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false
+  },
+  image_url: {
+    type: DataTypes.STRING
+  }
+}, {
+  tableName: 'products',
+  timestamps: true,
+  createdAt: 'created_at',
+  updatedAt: 'updated_at'
+});
+
+module.exports = Product;
